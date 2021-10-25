aee2fa88dd8978eb8dd130bbf89d27f62f1ca115
z4kn4fein <peter.csajtai@configcat.com>
2021-10-25 17:39:52 +0000

    Updating ConfigCat CLI Formula due to new release v1.2.2

--- Formula/configcat.rb
+++ Formula/configcat.rb
@@ -1,17 +1,17 @@
 class Configcat < Formula
   desc "The ConfigCat Command Line Interface allows you to interact with the ConfigCat Management API via the command line. It supports most functionality found on the ConfigCat Dashboard. You can manage ConfigCat resources like Feature Flags, Targeting / Percentage rules, Products, Configs, Environments, and more."
   homepage "https://configcat.com"
-  version "1.2.1"
+  version "1.2.2"
   license "MIT"
   bottle :unneeded
   
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/configcat/cli/releases/download/v1.2.1/configcat-cli_1.2.1_osx-x64.tar.gz"
-    sha256 "428e079cbfc3b31e9edf113d5224178031c8558b71cb95116cf5f511b448e92c"
+    url "https://github.com/configcat/cli/releases/download/v1.2.2/configcat-cli_1.2.2_osx-x64.tar.gz"
+    sha256 "8a4759693f3962eb88faa664d6b36701cd4acc6a00abfdd486bb4625a564e717"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/configcat/cli/releases/download/v1.2.1/configcat-cli_1.2.1_linux-x64.tar.gz"
-    sha256 "650ec14d445bfa4e691bf17e00ba68663189b3c89cbde2c2e115206d8eac4e96"
+    url "https://github.com/configcat/cli/releases/download/v1.2.2/configcat-cli_1.2.2_linux-x64.tar.gz"
+    sha256 "2f703e0c056634c550a623305a994f93d960d516282e00272b483c1284c1ce92"
   end
   
   def install
@@ -19,6 +19,6 @@ class Configcat < Formula
   end
 
   test do
-    assert_match "1.2.1", shell_output("#{bin}/configcat --version")
+    assert_match "1.2.2", shell_output("#{bin}/configcat --version")
   end
 end
