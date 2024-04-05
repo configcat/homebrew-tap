4efc027bc661a86a3154fcb9acbf7eb9c665be5d
z4kn4fein <peter.csajtai@configcat.com>
2024-04-05 13:53:04 +0000

    Updating ConfigCat CLI Formula due to new release v2.0.0

--- Formula/configcat.rb
+++ Formula/configcat.rb
@@ -1,20 +1,20 @@
 class Configcat < Formula
   desc "The ConfigCat Command Line Interface allows you to interact with the ConfigCat Management API via the command line. It supports most functionality found on the ConfigCat Dashboard. You can manage ConfigCat resources like Feature Flags, Targeting / Percentage rules, Products, Configs, Environments, and more."
   homepage "https://configcat.com"
-  version "1.9.2"
+  version "2.0.0"
   license "MIT"
   
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/configcat/cli/releases/download/v1.9.2/configcat-cli_1.9.2_osx-x64.tar.gz"
-    sha256 "a15bd9c4f0c0c3b293c6ad0fe244b92a6a77527a46481f3e0d2b75535f432d45"
+    url "https://github.com/configcat/cli/releases/download/v2.0.0/configcat-cli_2.0.0_osx-x64.tar.gz"
+    sha256 "63ea0c4de744ab009d28ed0164a3d8fa6c8db3ad1b2c8983fc73a7f1eb3de606"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/configcat/cli/releases/download/v1.9.2/configcat-cli_1.9.2_osx-arm64.tar.gz"
-    sha256 "f29a9692cacb13a65796357c2fbdc84302b7e2208b3a228f2a735e97d4385722"
+    url "https://github.com/configcat/cli/releases/download/v2.0.0/configcat-cli_2.0.0_osx-arm64.tar.gz"
+    sha256 "8c1cf8ae826d717ff1ca71dfd34b06d999728b44975f07380333e24b039468ec"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/configcat/cli/releases/download/v1.9.2/configcat-cli_1.9.2_linux-x64.tar.gz"
-    sha256 "c40e29a49a0a1fa16655c4d5021632fec707c7a470b5f2af35d96da56d1d9bd6"
+    url "https://github.com/configcat/cli/releases/download/v2.0.0/configcat-cli_2.0.0_linux-x64.tar.gz"
+    sha256 "3001d16f8b1ffdb047936dffd82427de63e1e0a89ed54ad7521f5f1f7d18ffbe"
   end
   
   def install
@@ -22,6 +22,6 @@ class Configcat < Formula
   end
 
   test do
-    assert_match "1.9.2", shell_output("#{bin}/configcat --version")
+    assert_match "2.0.0", shell_output("#{bin}/configcat --version")
   end
 end
