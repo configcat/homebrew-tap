e2b799dddc8547c3cf96be51b028c9c8c4af0363
Peter Csajtai <peter.csajtai@configcat.com>
2024-04-05 16:25:58 +0200

    Update configcat.rb

--- Formula/configcat.rb
+++ Formula/configcat.rb
@@ -4,13 +4,13 @@ class Configcat < Formula
   version "2.0.0"
   license "MIT"
 
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/configcat/cli/releases/download/v2.0.0/configcat-cli_2.0.0_osx-arm64.tar.gz"
-    sha256 "8c1cf8ae826d717ff1ca71dfd34b06d999728b44975f07380333e24b039468ec"
-  end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/configcat/cli/releases/download/v2.0.0/configcat-cli_2.0.0_osx-x64.tar.gz"
     sha256 "63ea0c4de744ab009d28ed0164a3d8fa6c8db3ad1b2c8983fc73a7f1eb3de606"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/configcat/cli/releases/download/v2.0.0/configcat-cli_2.0.0_osx-arm64.tar.gz"
+    sha256 "8c1cf8ae826d717ff1ca71dfd34b06d999728b44975f07380333e24b039468ec"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/configcat/cli/releases/download/v2.0.0/configcat-cli_2.0.0_linux-x64.tar.gz"
